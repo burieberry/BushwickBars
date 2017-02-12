b90f12f0453a8ffbc3f8a2aee5c057aa86a080eb
Burcu Noyan <burcunoyan@gmail.com>
2017-02-12 18:13:52 -0500

    move style to css file and script to app.js file; move boundary specifications to showListings func

---
+++ app.js
@@ -0,0 +1,85 @@
+var map; // global map object
+var markers = []; // global blank marker array
+
+/* Initiate map */
+function initMap() {
+  // Constructor to create a new map
+  map = new google.maps.Map(document.getElementById('map'), {
+    // center is Bushwich, Brooklyn
+    center: { lat: 40.694428, lng: -73.921286 },
+    zoom: 13
+  });
+
+  // Locations array for bars around Bushwick, Brooklyn
+  var locations = [
+    {title: 'Heavy Woods', location: {lat: 40.705606, lng: -73.921648}},
+    {title: 'Pearl\'s Social & Billy Club', location: {lat: 40.707065, lng: -73.921355}},
+    {title: 'Sally Roots', location: {lat: 40.702754, lng: -73.916373}},
+    {title: 'The Rookery Bar', location: {lat: 40.707404, lng: -73.922462}},
+    {title: 'Boobie Trap', location: {lat: 40.70015, lng: -73.91604}}
+  ];
+
+  var largeInfoWindow = new google.maps.InfoWindow();
+
+  // Use the location array to create an array of markers on initialize
+  for (var i = 0; i < locations.length; i++) {
+    // Get the position from location array
+    var position = locations[i].location;
+    var title = locations[i].title;
+
+    // Create a marker per location, and put into markers array
+    var marker = new google.maps.Marker({
+      position: position,
+      map: map,
+      title: title,
+      animation: google.maps.Animation.DROP,
+      id: i
+    });
+
+    // Push the marker into array of markers.
+    markers.push(marker);
+
+    // Create onclick event to open an infowindow for each marker
+    marker.addListener('click', function() {
+      populateInfoWindow(this, largeInfoWindow);
+    });
+  }
+
+  // Add event-listeners for show/hide listings buttons
+  document.getElementById('show-listings').addEventListener('click', showListings);
+  document.getElementById('hide-listings').addEventListener('click', hideListings);
+}
+
+/* Loop through markers array and show all markers */
+function showListings() {
+  var bounds = new google.maps.LatLngBounds();
+
+  // Extend the boundaries of the map for each marker and display each marker
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+    bounds.extend(markers[i].position);
+  }
+  map.fitBounds(bounds);
+}
+
+/* Loop through markers and hide all markers */
+function hideListings() {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+}
+
+/* Populate infowindow when a marker is clicked */
+function populateInfoWindow(marker, infoWindow) {
+  // Check to make sure the infowindow is not already opened on this marker
+  if (infoWindow.marker != marker) {
+    infoWindow.marker = marker;
+    infoWindow.setContent('<div>' + marker.title + '</div>');
+    infoWindow.open(map, marker);
+
+    // Make sure the marker property is cleared if the infowindow is closed
+    infoWindow.addListener('closeclick', function() {
+      infoWindow.setMarker = null;
+    });
+  }
+}
